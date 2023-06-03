8ca6972ef5d53775c93b7151886d087b13950416
ramazanozersf <brsheres@gmail.com>
2023-06-02 21:41:52 -0700

    LEET-5: add auth page.

--- my-app/src/pages/_app.tsx
+++ my-app/src/pages/_app.tsx
@@ -1,8 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import { RecoilRoot } from "recoil";
-
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
